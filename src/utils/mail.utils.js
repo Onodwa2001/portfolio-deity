6897dce879b5df589a4afe6466dc5fdf7f3be959
= <=>
2024-08-19 15:57:21 +0200

    Attempt to fix email service in prod

--- src/utils/mail.utils.js
+++ src/utils/mail.utils.js
@@ -5,10 +5,17 @@ const transport = nodemailer.createTransport({
     host: process.env.MAIL_HOST,
     port: process.env.MAIL_PORT,
     secure: true,
+    service: 'gmail',
+    logger: true,
+    debug: true,
+    secureConnection: false,
     auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASSWORD
     },
+    tls: {
+        rejectUnauthorized: true
+    }
 })
 
 
