6b215714db30314584b5d349af647a6dd51644bf
= <=>
2024-08-19 16:13:01 +0200

    Attempt to fix email service in prod 4

--- src/utils/mail.utils.js
+++ src/utils/mail.utils.js
@@ -3,16 +3,16 @@ import Mail from 'nodemailer/lib/mailer'
 
 
 const transport = nodemailer.createTransport({
-    host: process.env.MAIL_HOST,
-    port: process.env.MAIL_PORT,
+    host: process.env.NEXT_PUBLIC_MAIL_HOST,
+    port: process.env.NEXT_PUBLIC_MAIL_PORT,
     secure: true,
     service: 'gmail',
     logger: true,
     debug: true,
     secureConnection: false,
     auth: {
-        user: "onodwasiyotula7@gmail.com",
-        pass: "vwrbldxwgttfduot"
+        user: process.env.NEXT_PUBLIC_MAIL_USER,
+        pass: process.env.NEXT_PUBLIC_MAIL_PASSWORD
     },
     tls: {
         rejectUnauthorized: true
